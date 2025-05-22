12b1ad6d9c8cce9fbdfd24c09452ee581b1ba554
Md.Rakibul islam <user@MDRakibul-islam.local>
2025-05-22 20:31:34 +0600

    Update repo with latest changes

--- jersi/settings.py
+++ jersi/settings.py
@@ -21,7 +21,7 @@ STATIC_URL = '/static/'
 
 from pathlib import Path
 
-DEBUG = False
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,10 +139,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Serve static files in development
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Default primary key field type
